af931df2a8cdd285433f4437d82c2151f4e9dfb2
aryngazydastan <aryngazy.dastan@gmail.com>
2015-09-21 00:20:30 +0600

    Update model post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
 	has_attached_file :file
-	do_not_validate_attachment_file_type :file
+	do_not_validate_attachment_file_type :file, :storage => :s3,
+            :bucket  => ENV['amazonaws1']
 end

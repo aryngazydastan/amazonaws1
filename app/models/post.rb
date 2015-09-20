f6efa504541707b929b3087e10aea337e7dfa378
aryngazydastan <aryngazy.dastan@gmail.com>
2015-09-21 00:24:56 +0600

    Update model post with all credentials

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,12 @@
 class Post < ActiveRecord::Base
 	has_attached_file :file
-	do_not_validate_attachment_file_type :file, :storage => :s3,
-            :bucket  => ENV['amazonaws1']
+	do_not_validate_attachment_file_type :file, 
+			:storage => :s3,
+            :bucket  => ENV['MY_BUCKET_NAME'],
+            :s3_credentials => {
+                    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+                    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+                },
+                :s3_permissions => 'private',
+                :url => ":s3_domain_url"
 end
